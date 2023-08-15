f9a85c88374a824e25a12453172f8a689e4e454a
Janice Wan <janicepcwan@gmail.com>
2023-08-16 02:20:45 +0800

    Initial Commit

---
+++ FinalProjG3/FinalProjG3App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjG3App.swift
+//  FinalProjG3
+//
+//  Created by Conrad Wan on 16/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjG3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
